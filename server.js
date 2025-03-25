0ea07b5279e81b9740d69b435bb99c64d2df4324
fpaiman <Fpaiman@hotmail.com>
2025-03-24 22:43:14 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mariadb = require("mariadb");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());  // Enable CORS for frontend communication
+app.use(bodyParser.json());
+
+// MariaDB Connection Pool
+const pool = mariadb.createPool({
+    host: "127.0.0.1",   // Change to your MariaDB server IP if remote
+    user: "admin",
+    password: "Binoo-143",
+    database: "stockfoliodb",
+    connectionLimit: 10,
+acquireTimeout: 20000 
+});
+
+// API Route to Insert Data into MariaDB from register user 
+//for other pages we use /stocks or /bonds or /real estate
+//then check db for for items to insert
+app.post("/register", async (req, res) => {
+
+console.log(req.body);
+const users = req.body; // Extract the array from request body
+console.log(users);
+    // Extract first user's values
+    //const id1 = users[0].id;
+    const username = users[0].username;
+    const password = users[0].password;
+    const firstname = users[0].firstname;
+    const lastname = users[0].lastname;
+    const email = users[0].email;
+
+const today = new Date();
+const date=today.toISOString().split("T")[0];
+ 
+  let conn;
+  try {
+
+     conn = await pool.getConnection();
+console.log("con");       
+await conn.query("INSERT INTO user (first_name, last_name, email, username, password, account_date ) VALUES (?, ?, ?, ?, ?, ?)", [firstname, lastname, email, username, password, date]);
+       res.send("User registered successfully!");
+} catch (err) {
+ res.status(500).send("Error: " + err.message);
+ } finally {
+ if (conn) conn.release();
+ }
+//code to test connection only
+ // let conn;
+   //   try {
+     //   conn = await pool.getConnection();
+       // console.log("connected ! connection id is " + conn.threadId);
+       // await conn.release(); //release to pool
+     // } catch (err) {
+       // console.log("not connected due to error: " + err);
+     // }
+//code to test connection ends here
+
+});
+
+// Start the Server
+app.listen(3000, () => {
+    console.log("Server running on http://0.0.0.0:3000");
+});
